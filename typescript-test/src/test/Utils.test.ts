4c4a04fc105d2fdd11c1b7f0414c5c7e8e904b1e
Boobalan S <boobal.sachin@gmail.com>
2023-07-30 20:08:25 +0530

    multiple-tests-structure

--- typescript-test/src/test/Utils.test.ts
+++ typescript-test/src/test/Utils.test.ts
@@ -9,26 +9,36 @@ describe("Utils test suite", () => {
     expect(actual).toBe(expected);
   });
 
-  it("should return string info when valid string is passed", () => {
-    const actual = getStringInfo("Test");
+  describe("when getStringInfo is called with valid string, then", () => {
+    it("should return right length", () => {
+      const actual = getStringInfo("Test");
 
-    expect(actual.upperCase).toBe("TEST");
+      expect(actual.length).toBe(4);
+    });
+    it("should return right upper case", () => {
+      const actual = getStringInfo("Test");
 
-    expect(actual.lowerCase).toBe("test");
+      expect(actual.upperCase).toBe("TEST");
+    });
+    it("should return right lower case", () => {
+      const actual = getStringInfo("Test");
 
-    expect(actual.length).toBe(4);
+      expect(actual.lowerCase).toBe("test");
+    });
+    it("should return right characters array", () => {
+      const actual = getStringInfo("Test");
 
-    expect(actual.characters).toEqual(["T", "e", "s", "t"]);
-    expect(actual.characters).toHaveLength(4);
-    expect(actual.characters).toContain("T");
-    expect(actual.characters).toEqual(
-      expect.arrayContaining(["e", "s", "t", "T"])
-    );
+      expect(actual.characters).toEqual(["T", "e", "s", "t"]);
+      expect(actual.characters).toHaveLength(4);
+      expect(actual.characters).toContain("T");
+      expect(actual.characters).toEqual(
+        expect.arrayContaining(["e", "s", "t", "T"])
+      );
+    });
+    it("should return defined extra info", () => {
+      const actual = getStringInfo("Test");
 
-    expect(actual.extraInfo).toEqual({});
-    expect(actual.extraInfo).not.toBe(undefined);
-    expect(actual.extraInfo).not.toBeUndefined();
-    expect(actual.extraInfo).toBeDefined();
-    expect(actual.extraInfo).toBeTruthy;
+      expect(actual.extraInfo).toBeTruthy;
+    });
   });
 });
