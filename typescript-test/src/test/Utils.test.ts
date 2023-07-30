fb0157a6307e06745625f475c8fadbf16566b874
Boobalan S <boobal.sachin@gmail.com>
2023-07-30 20:01:42 +0530

    jest-assertions-and-matchers

--- typescript-test/src/test/Utils.test.ts
+++ typescript-test/src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUppercase } from "../app/Utils";
+import { getStringInfo, toUppercase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of string when valid string is passed", () => {
@@ -7,5 +7,28 @@ describe("Utils test suite", () => {
     const actual = toUppercase("abc");
 
     expect(actual).toBe(expected);
+  });
+
+  it("should return string info when valid string is passed", () => {
+    const actual = getStringInfo("Test");
+
+    expect(actual.upperCase).toBe("TEST");
+
+    expect(actual.lowerCase).toBe("test");
+
+    expect(actual.length).toBe(4);
+
+    expect(actual.characters).toEqual(["T", "e", "s", "t"]);
+    expect(actual.characters).toHaveLength(4);
+    expect(actual.characters).toContain("T");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["e", "s", "t", "T"])
+    );
+
+    expect(actual.extraInfo).toEqual({});
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy;
   });
 });
