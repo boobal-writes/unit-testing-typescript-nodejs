fb6c8b6c8b42a938a7c4f7ebc0742bd9b73a9268
Boobalan S <boobal.sachin@gmail.com>
2023-07-30 19:37:51 +0530

    structure-of-an-unit-test

--- typescript-test/src/test/Utils.test.ts
+++ typescript-test/src/test/Utils.test.ts
@@ -1,8 +1,11 @@
 import { toUppercase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("should return uppercase", () => {
-    const result = toUppercase("abc");
-    expect(result).toBe("ABC");
+  it("should return uppercase of string when valid string is passed", () => {
+    const expected = "ABC";
+
+    const actual = toUppercase("abc");
+
+    expect(actual).toBe(expected);
   });
 });
