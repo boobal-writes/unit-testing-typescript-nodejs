2f44ca6da5b29fa398549a7561ba442142b5645b
Boobalan S <boobal.sachin@gmail.com>
2023-07-31 22:26:16 +0530

    iteration-1

--- typescript-test/src/app/pass_checker/PasswordChecker.ts
+++ typescript-test/src/app/pass_checker/PasswordChecker.ts
@@ -1,3 +1,17 @@
+export enum PasswordError {
+  SHORT = "Password is shorter!",
+  NO_UPPER_CASE = "Upper case letter is required!",
+  NO_LOWER_CASE = "Lower case letter is required!",
+}
+export interface CheckResult {
+  valid: boolean;
+  reasons: PasswordError[];
+}
 export class PasswordChecker {
-  public checkPassword() {}
+  public checkPassword(password: string): boolean {
+    if (password.length < 8) return false;
+    if (password == password.toLowerCase()) return false;
+    if (password == password.toUpperCase()) return false;
+    return true;
+  }
 }
--- typescript-test/src/test/pass_checker/PasswordChecker.test.ts
+++ typescript-test/src/test/pass_checker/PasswordChecker.test.ts
@@ -5,7 +5,32 @@ describe("Password checker test suite", () => {
   beforeEach(() => {
     passwordChecker = new PasswordChecker();
   });
-  it("should do nothing for the moment", () => {
-    passwordChecker.checkPassword();
+  it("should return false, when password is having less than 8 characters", () => {
+    const password = "abcd";
+
+    const actualResult = passwordChecker.checkPassword(password);
+
+    expect(actualResult).toBe(false);
+  });
+  it("should return true, when valid password is passed", () => {
+    const password = "Abcdefgh";
+
+    const actualResult = passwordChecker.checkPassword(password);
+
+    expect(actualResult).toBe(true);
+  });
+  it("should return false, when password with no upper case is passed", () => {
+    const password = "abcdefgh";
+
+    const actualResult = passwordChecker.checkPassword(password);
+
+    expect(actualResult).toBe(false);
+  });
+  it("should return false, when password with no lower case is passed", () => {
+    const password = "ABCDEFGH";
+
+    const actualResult = passwordChecker.checkPassword(password);
+
+    expect(actualResult).toBe(false);
   });
 });
