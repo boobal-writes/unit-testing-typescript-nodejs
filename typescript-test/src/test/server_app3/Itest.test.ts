3607b124e60e30aaa856d86e627390e73d0750bd
Boobalan S <boobal.sachin@gmail.com>
2023-11-25 22:26:50 +0530

    integration test with makeAwesomeRequest

--- typescript-test/src/test/server_app3/Itest.test.ts
+++ typescript-test/src/test/server_app3/Itest.test.ts
@@ -3,6 +3,7 @@ import {
   HTTP_METHODS,
 } from "../../app/server_app/model/ServerModel";
 import { Server } from "../../app/server_app/server/Server";
+import { makeAwesomeRequest } from "./utils/http-client";
 
 describe("Server app integration tests", () => {
   let sut: Server;
@@ -20,7 +21,7 @@ describe("Server app integration tests", () => {
     password: "somePassword",
   };
 
-  it("should register new user", async () => {
+  it("should register new user with fetch library", async () => {
     const result = await fetch("http://localhost:8080/register", {
       method: HTTP_METHODS.POST,
       body: JSON.stringify(someUser),
@@ -29,5 +30,20 @@ describe("Server app integration tests", () => {
 
     expect(result.status).toBe(HTTP_CODES.CREATED);
     expect(resultBody.userId).toBeDefined();
+  });
+
+  it("should register new user with makeAwesomeRequest", async () => {
+    const result = await makeAwesomeRequest(
+      {
+        host: "localhost",
+        port: "8080",
+        method: HTTP_METHODS.POST,
+        path: "/register",
+      },
+      someUser
+    );
+
+    expect(result.statusCode).toBe(HTTP_CODES.CREATED);
+    expect(result.body.userId).toBeDefined();
   });
 });
