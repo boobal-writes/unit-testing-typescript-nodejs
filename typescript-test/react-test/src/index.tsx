8290e84ddf565c2657a213dec7d9323c4454963c
Boobalan S <boobal.sachin@gmail.com>
2023-11-26 11:46:42 +0530

    add login component with service

--- typescript-test/react-test/src/index.tsx
+++ typescript-test/react-test/src/index.tsx
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import App2 from "./App2";
+import LoginComponent from "./LoginComponent";
+import LoginService from "./services/LoginService";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App2 />);
+
+const loginService = new LoginService();
+const setToken = (token: string) => {
+  console.log(`Token is ${token}`);
+};
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />);
